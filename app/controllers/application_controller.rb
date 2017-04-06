48da69be57acdd3d293150bf7fdd59ee3cfb03a5
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-04-06 20:28:10 +0000

    Refactor shared method to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_not_found(status = :not_found)
+    if status == :forbidden
+      render file: 'public/403.html', status: status
+    else
+      render file: 'public/404.html', status: status
+    end
+  end
+
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def new
     @comment = Comment.new
   end
 
   def create
-    return redirect_to new_user_session_path if current_user.nil?
     @gram = Gram.find_by_id(params[:gram_id])
-    return render plain: '404 Not Found', status: :not_found if @gram.nil?
+    return render_not_found if @gram.blank?
     @gram.comments.create(comment_params.merge(user: current_user))
     redirect_to root_path
   end
