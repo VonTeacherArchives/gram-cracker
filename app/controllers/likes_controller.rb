446bb6add8dc28128180a51cf06f6c8d59a2a637
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-04-09 04:42:09 +0000

    Resolve dynamic update, but need fix full-page refresh

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,10 @@ class LikesController < ApplicationController
   def create
     gram = Gram.find(params[:gram_id])
     current_user.likes.create(gram: gram)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
 end
